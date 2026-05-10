db33f77cb77cf14c51966695eaac87c0417ff4b6
himanshu4170 <himanshu4170@gmail.com>
2026-05-10 16:54:41 +0530

    secured admin complaint routes

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use(express.json());
 
 // test route
