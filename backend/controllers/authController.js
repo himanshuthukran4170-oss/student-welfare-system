dbf4dc9d75d95cc4f41a119dd311ee1d868f93e7
himanshu4170 <himanshu4170@gmail.com>
2026-05-10 17:07:03 +0530

    added role in login response

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -83,8 +83,14 @@ const loginUser = async (req,res) => {
         );
 
         res.status(200).json({
-            success:true,
-            token
+            success: true,
+            token,
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
         });
 
     }
