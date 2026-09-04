5a777c7be28db76b3256367fbf49817c13c3f313
Himanshu Thukran <himanshuthukran4170@gmail.com>
2026-09-04 12:13:31 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -23,22 +23,7 @@ const registerUser = async (req,res) => {
         const hashedPassword = await bcrypt.hash(password,10);
 
         const user = await User.create({
-            name,
-            email,
-            password:hashedPassword
-        });
-
-        res.status(201).json({
-            success:true,
-            message:"User registered successfully",
-            user
-        });
-
-    }
-    catch(error){
-        res.status(500).json({
-            success:false,
-            message:error.message
+        
         });
     }
 };
